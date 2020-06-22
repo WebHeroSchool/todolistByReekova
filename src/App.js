b8541da952e3bb00067fc9c85c66bb0470a4fc8d
Maria Rykova <maria.nigay@yandex.ru>
2020-06-22 16:35:16 +0300

    *task14*

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const name = 'Maria Reekova';
+const weather = true;
+const activity = true;
+
 function App() {
   return (
     <div className="App">
@@ -10,7 +14,29 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Hello! Let's make together a new app! :) Let's go! </p>
+        <div>{name}</div>
+        <div> Age {30} </div>
+        <div> Sum {10 + 5} </div>
+        <p>
+          {undefined}
+          {null}
+          {false}
+          {true}
+        </p>
+        <p style={{
+          color: 'yellow',
+          fontSize: 50,
+          fontWeight: 700
+        }}>
+        Hello! Let's make together a new app! :) Let's go!
+        </p>
+        <p style={{
+          color: 'yellow',}}>
+          {weather && 'Weather is sunny today'}
+          <div>
+          {activity ? "Let's go to swim" : "Let's stay at home"}
+          </div>
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
