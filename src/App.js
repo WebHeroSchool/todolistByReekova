ada72115c8b50095526fe2211460c8cb178e7eb9
Maria Rykova <maria.nigay@yandex.ru>
2020-07-08 13:05:43 +0300

    task17

--- src/App.js
+++ src/App.js
@@ -1,4 +1,15 @@
 import React from 'react';
 
-const element = (<div>Привет! Как дела?</div>);
-export {element}
+const ItemList = () => (<ul>
+  <li>1.Сделать зарядку</li>
+  <li>2.Погулять с собакой</li>
+</ul>);
+
+const title = (<h1> ToDoList for today </h1>);
+
+const App = () => (<div>
+  {title}
+  <ItemList />
+</div>);
+
+export default App
