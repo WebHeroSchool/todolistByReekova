406e6fbda6e0c86cd99c958318e202dbf7a10c18
Maria Rykova <maria.nigay@yandex.ru>
2020-07-09 17:51:21 +0300

    task25

--- src/components/InputItem/index.js
+++ src/components/InputItem/index.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 
-const InputItem = () => (<span>Здесь скоро будет input item</span>);
+const InputItem = () => (<span>
+  <TextField
+          id="standard-full-width"
+          label=":)"
+          style={{ margin: 8 }}
+          placeholder="Новое задание"
+          fullWidth
+          margin="normal"
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+  </span>);
 
 export default InputItem;
