e22a723f4e23b9e6886e13bad5ec8584cbdd120e
Maria Rykova <maria.nigay@yandex.ru>
2020-07-10 16:04:35 +0300

    task27

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,33 +4,35 @@ import Footer from '../Footer';
 import InputItem from '../InputItem';
 import styles from './App.module.css';
 
-const App = () => {
-  const tasks = [
-    {
-      value: 'Сделать зарядку',
-      isDone: false
-    },
-    {
-      value: 'Погулять с собакой',
-      isDone: true
-    },
-    {
-      value: 'Приготовить завтрак',
-      isDone: true
-    },
-    {
-      value: 'Работать-работать-работать',
-      isDone: false
-    }
-  ];
+class App extends React.Component {
+  render() {
+    const tasks = [
+      {
+        value: 'Сделать зарядку',
+        isDone: false
+      },
+      {
+        value: 'Погулять с собакой',
+        isDone: true
+      },
+      {
+        value: 'Приготовить завтрак',
+        isDone: true
+      },
+      {
+        value: 'Работать-работать-работать',
+        isDone: false
+      }
+    ];
 
-  return (
-    <div className={styles.wrap}>
-      <h1 className={styles.title}>Важные дела</h1>
-      <InputItem />
-      <ItemList tasks={tasks}/>
-      <Footer count={tasks.length}/>
-    </div>);
+    return (
+      <div className={styles.wrap}>
+        <h1 className={styles.title}>Важные дела</h1>
+        <InputItem />
+        <ItemList tasks={tasks}/>
+        <Footer count={tasks.length}/>
+      </div>);
+  }
 }
 
 export default App;
